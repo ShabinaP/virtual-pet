a31ac0ec6261dc5a4d82c5fe45a2b3e6cf724164
Shabina <shabina-patel@hotmail.com>
2021-07-11 14:51:23 +0100

    added functionality and refactored some code

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -36,4 +36,24 @@ describe("growup", () => {
     });
 });
 
+describe("walk", () => {
+    it("increases fitness by 4", () => {
+        const pet = new Pet("Fido");
+        pet.fitness = 4;
+        pet.walk();
+        expect(pet.fitness).toEqual(8)
+    });
+    it("increases fitness to a maxiumum of 10", () => {
+        const pet = new Pet("Fido");
+        pet.fitness = 8;
+        pet.walk();
+        expect(pet.fitness).toEqual(10)
+    });
+
+});
+
+
+
+
+
     
--- src/pet.js
+++ src/pet.js
@@ -1,8 +1,10 @@
+const MAXIMUM_FITNESS = 10;
+
 function Pet (name) {
 this.name = name;
 this.age = 0;
 this.hunger =0;
-this.fitness =10;
+this.fitness = MAXIMUM_FITNESS;
 }
 
 Pet.prototype.growUp = function () {
@@ -12,5 +14,12 @@ Pet.prototype.growUp = function () {
     this.fitness -=3;
 
 }
+
+Pet.prototype.walk = function () {
+    if ((this.fitness + 4) <= MAXIMUM_FITNESS) {
+        this.fitness +=4;
+    }
+    else {this.fitness = MAXIMUM_FITNESS} 
+};
 
 module.exports = Pet;
