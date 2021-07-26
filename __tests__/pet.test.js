8a0ed9bca2205a50d88c165e99098406b810b3fe
Shabina <shabina-patel@hotmail.com>
2021-07-26 17:16:16 +0100

    tidied up tests

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -12,7 +12,7 @@ describe ("constructor", () => {
 expect(pet.name).toEqual("Fido");
     })
 
-    it("initial age is 0", () => {
+    it("sets initial age at 0", () => {
         const pet = new Pet("Fido");
     expect(pet.age).toEqual(0)
     })
